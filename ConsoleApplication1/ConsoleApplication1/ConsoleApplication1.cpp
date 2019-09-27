feb72c5b7af24231e8e15d87ebc8827f06792d78
Anziiiii2 <antti.isopahkala00@gmail.com>
2019-09-27 19:04:17 +0300

    Generaotr fix
    Array generator cant have 2 same compinations

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,6 +11,9 @@ void CreateArray() {
 	string alphabet1[26] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
 	//string *Alphabet = new string[length]
 	string alphabet[2][26];
+	int num1copy = 0;
+	int num2copy = 0;
+	int num3copy = 0;
 	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 0; j < 26; j++)
@@ -20,17 +23,21 @@ void CreateArray() {
 				cout << alphabet[i][j] << endl;
 			}
 			else {
+				createNewNumbers:
 				int num1 = GetRandom();
 				int num2 = GetRandom();
 				int num3 = GetRandom();
-
-				alphabet[i][j] += alphabet1[num1];
-				alphabet[i][j] += alphabet1[num2];
-				alphabet[i][j] += alphabet1[num3];
-				for (int k = 0; k < 26; k++)
-				{
-
+				if (num1copy != num1 && num2copy != num2 && num3copy != num3) {
+					alphabet[i][j] += alphabet1[num1];
+					alphabet[i][j] += alphabet1[num2];
+					alphabet[i][j] += alphabet1[num3];
 				}
+				else {
+					goto createNewNumbers;
+				}
+				num1copy = num1;
+				num2copy = num2;
+				num3copy = num3;
 				cout << alphabet[i][j] << endl;
 			}
 		}
