c3892a8424822e10fc89e47800b35958bc479e8e
Anziiiii2 <antti.isopahkala00@gmail.com>
2019-09-27 12:43:35 +0300

    Added base project

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "encryperDecrypter.h"
+#include <stdlib.h>
+using namespace std;
+void main() {
+	CreateArray();
+}
+void CreateArray() {
+	int length = 23;
+	string alphabet1[26] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+	//string *Alphabet = new string[length]
+	string alphabet[2][26];
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 23; j++)
+		{
+			if (i == 0) {
+				alphabet[i][j] = alphabet1[j];
+				cout << alphabet[i][j] << endl;
+			}
+			else {
+				int num1 = GetRandom();
+				int num2 = GetRandom();
+				int num3 = GetRandom();
+				alphabet[i][j] += alphabet1[num1];
+				alphabet[i][j] += alphabet1[num2];
+				alphabet[i][j] += alphabet1[num3];
+				cout << alphabet[i][j] << endl;
+			}
+		}
+	}
+}
+int GetRandom() {
+	int max = 26;
+	int min = 0;
+	return rand() % (max - min);
+}
