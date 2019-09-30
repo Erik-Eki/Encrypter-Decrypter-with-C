6561dfc4c6841473411048ecb8d265131ac3a772
Anziiiii2 <antti.isopahkala00@gmail.com>
2019-09-30 14:03:43 +0300

    MadeEncrypter work

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
 #include <string>
-#include "encryperDecrypter.h"
 #include <stdlib.h>
+#include <fstream>
+#include <vector>
+
 using namespace std;
-void main() {
-	CreateArray();
+string encryptedchars1[] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+string encrupedchar11 = "abcdefghijklmnopqrstuvwxyz";
+string encryptedchars[26] = { "abd", "asd","kod", "uds", "kds", "dse", "dka", "yhr", "daj", "fkl", "enr","kof", "kta", "knh", "dak", "dan" , "mal", "ojn", "amm", "evn", "dmf", "ekt", "aot", "nge", "dao", "dak" };
+string encryptedMessage = "";
+void CreateArray();
+int GetRandom();
+void main()
+{
+	setlocale(LC_ALL, "Finnish");
+	//CreateArray();
+
+	string s;
+	int size = 0, op;
+	cout << "Syötä salainen viesti: ";
+	cin >> s;
+	size = s.length();
+
+	for (int i = 0; i < size; i++)
+	{
+
+		int found = encrupedchar11.find(s[i]);
+		encryptedMessage += encryptedchars[found];
+		//if (found != string::npos) {
+		//}
+	}
+
+	cout << encryptedMessage << endl;
+	cout << "Viesti enkryptattu!" << endl;
+/*
+	cout << "Paina 1 siirtyäksesi enkryptaukseen." << endl;
+	cout << "Valinta: ";
+	cin >> op;
+	switch (op) {
+	case 1:
+		
+
+		break;
+	default:
+		cout << "Väärin kirjoitettu tymhä.";
+
+	}*/
+	
 }
 void CreateArray() {
-	int length = 26;
-	string alphabet1[26] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
-	//string *Alphabet = new string[length]
-	string alphabet[2][26];
+
 	int num1copy = 0;
 	int num2copy = 0;
 	int num3copy = 0;
-	for (int i = 0; i < 2; i++)
+	for (size_t i = 0; i < 26; i++)
 	{
-		for (int j = 0; j < 26; j++)
-		{
-			if (i == 0) {
-				alphabet[i][j] = alphabet1[j];
-				cout << alphabet[i][j] << endl;
-			}
-			else {
-				createNewNumbers:
-				int num1 = GetRandom();
-				int num2 = GetRandom();
-				int num3 = GetRandom();
-				if (num1copy != num1 && num2copy != num2 && num3copy != num3) {
-					alphabet[i][j] += alphabet1[num1];
-					alphabet[i][j] += alphabet1[num2];
-					alphabet[i][j] += alphabet1[num3];
-				}
-				else {
-					goto createNewNumbers;
-				}
-				num1copy = num1;
-				num2copy = num2;
-				num3copy = num3;
-				cout << alphabet[i][j] << endl;
-			}
+	createNewNumbers:
+		int num1 = GetRandom();
+		int num2 = GetRandom();
+		int num3 = GetRandom();
+
+		if (num1copy != num1 && num2copy != num2 && num3copy != num3) {
+			encryptedchars[i] += encryptedchars1[num1];
+			encryptedchars[i] += encryptedchars1[num2];
+			encryptedchars[i] += encryptedchars1[num3];
 		}
+		else {
+			goto createNewNumbers;
+		}
+		num1copy = num1;
+		num2copy = num2;
+		num3copy = num3;
+
 	}
-}
+	}
 int GetRandom() {
 	int max = 26;
 	int min = 0;
--- ConsoleApplication1/ConsoleApplication1/encryperDecrypter.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void CreateArray();
-int GetRandom();
-void main();
