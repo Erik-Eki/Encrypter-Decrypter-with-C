1687bdbd6fbb111e9252f507dc27b54eaec2d3a4
erik <erikhuuskonen@kamk.fi>
2019-10-04 10:41:56 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,10 +5,11 @@
 #include <vector>
 
 using namespace std;
-string encryptedchars1[] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+string encryptedchars1[26] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
 string encrupedchar11 = "abcdefghijklmnopqrstuvwxyz";
 string encryptedchars[26] = { "abd", "asd","kod", "uds", "kds", "dse", "dka", "yhr", "daj", "fkl", "enr","kof", "kta", "knh", "dak", "dan" , "mal", "ojn", "amm", "evn", "dmf", "ekt", "aot", "nge", "dao", "dak" };
 string encryptedMessage = "";
+string decryptedMessage = "";
 void CreateArray();
 int GetRandom();
 void main()
@@ -19,7 +20,7 @@ void main()
 	int size = 0, op;
 
 
-	cout << "Paina 1 siirtyäksesi enkryptaukseen." << endl;
+	cout << "Paina 1 siirtyäksesi enkryptaukseen, 2 dekryptaukseen." << endl;
 	cout << "Valinta: ";
 	cin >> op;
 	switch (op) {
@@ -41,6 +42,19 @@ void main()
 	cout << "Viesti enkryptattu!" << endl;
 
 		break;
+	case 2:
+		cout << "Syötä enkryptattu viesti: ";
+		cin >> s;
+		size = s.length();
+
+		for (int i = 0; i < size; i++)
+		{
+			int found = encryptedchars[26].find(s[i]);
+			decryptedMessage += encryptedchars1[found];
+		}
+		cout << decryptedMessage << endl; //Ei jostain syystä printtaa mitään >:(
+		cout << "Viesti decryptattu!" << endl;
+			break;
 	default:
 		cout << "Väärin kirjoitettu tymhä.";
 
